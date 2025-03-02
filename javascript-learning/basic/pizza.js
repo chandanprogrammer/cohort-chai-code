dde9f390bbb5bd206ba469c4c5cf974019d7289f
chandan <chandankumar070501@gmail.com>
2025-03-02 17:07:07 +0530

    add: array and array method

--- javascript-learning/basic/pizza.js
+++ javascript-learning/basic/pizza.js
@@ -1,10 +1,16 @@
-let numberOfGuest = 4;
+let numberofGuest = -1;
 
 let pizzaSize;
 
-if (numberOfGuest <= 2) {
+// small <= 2
+// medium <= 5
+//large
+
+// 5 <= 5
+
+if (-1 <= 2) {
   pizzaSize = "small";
-} else if (numberOfGuest <= 5) {
+} else if (numberofGuest <= 5) {
   pizzaSize = "medium";
 } else {
   pizzaSize = "large";
